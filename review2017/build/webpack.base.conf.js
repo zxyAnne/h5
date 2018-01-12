a594f46756ab78f7792af46e695d580a4f6ff67a
ht1131589588 <1131589588@qq.com>
2018-01-12 11:19:22 +0800

    年度总结调整资源路径

--- review2017/build/webpack.base.conf.js
+++ review2017/build/webpack.base.conf.js
@@ -17,7 +17,7 @@ module.exports = {
     path: config.build.assetsRoot,
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
-      ? 'https://h5.mindcherish.com/'
+      ? 'review2017' + config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
   resolve: {
